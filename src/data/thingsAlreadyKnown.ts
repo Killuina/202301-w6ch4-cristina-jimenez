99e0a4be100ab531fdbf47d08d2f2d9963a3a3e8
Cristina <cristinajmartin98@gmail.com>
2023-02-16 19:36:46 +0100

    Add ThingsAlreadyKnown and its types

---
+++ src/data/thingsAlreadyKnown.ts
@@ -0,0 +1,6 @@
+const thingsAlreadyKnown = [
+  {
+    id: 1,
+    thing: "TODO lo relacionado con desarrollo Front-End",
+  },
+];
---
+++ src/data/types.ts
@@ -0,0 +1,7 @@
+interface ThingDataStructure {
+  name: string;
+}
+
+interface ThingStructure extends ThingDataStructure {
+  id: number;
+}
